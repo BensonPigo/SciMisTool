ced0dea2b068421993b9f1af4d6ab7a0a27f8eb2
BensonPigo <38678665+BensonPigo@users.noreply.github.com>
2025-06-09 09:13:46 +0800

    Refactor service log handler to pick client by factory

---
+++ SciTaipeiTool/internal/handler/serviceloghandler.go
@@ -0,0 +1,65 @@
+package handler
+
+import (
+    "context"
+    "encoding/json"
+    "net/http"
+    "time"
+
+    "SciTaipeiTool/internal/ftygrpc"
+    pb "SciTaipeiTool/proto/taskexecutor"
+)
+
+type ServiceLogHandler struct {
+    GRpcClients []*ftygrpc.Client
+}
+
+func (h *ServiceLogHandler) GetServiceLog(w http.ResponseWriter, r *http.Request) {
+    serviceName := r.URL.Query().Get("serviceName")
+    rawDate := r.URL.Query().Get("logDate")
+    factoryID := r.URL.Query().Get("factoryId")
+
+    if factoryID == "" {
+        http.Error(w, "missing factoryId", http.StatusBadRequest)
+        return
+    }
+
+    logDate, err := time.Parse("2006-01-02", rawDate)
+    if err != nil {
+        http.Error(w, "logDate \u683c\u5f0f\u932f\u8aa4", http.StatusBadRequest)
+        return
+    }
+
+    var client *ftygrpc.Client
+    for _, c := range h.GRpcClients {
+        if c.FactoryId == factoryID {
+            client = c
+            break
+        }
+    }
+    if client == nil {
+        http.Error(w, "invalid factoryId", http.StatusBadRequest)
+        return
+    }
+
+    req := &pb.GetServiceLogRequest{
+        ServiceName: serviceName,
+        Date:        logDate.Format("2006-01-02"),
+    }
+    resp, err := client.GetServiceLog(context.Background(), req)
+    if err != nil {
+        http.Error(w, "gRPC \u547c\u53eb\u5931\u6557: "+err.Error(), http.StatusInternalServerError)
+        return
+    }
+    if resp.GetLogContent() == "" {
+        http.Error(w, "\u627e\u4e0d\u5230\u5c0d\u61c9\u7684 log", http.StatusNotFound)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(map[string]string{
+        "serviceName": serviceName,
+        "logDate":     logDate.Format("2006-01-02"),
+        "content":     resp.GetLogContent(),
+    })
+}
